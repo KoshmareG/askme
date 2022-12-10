6c6f6c61d4960b46d3a36d58a7ad62636a5b5747
KoshmareG <koshmareg@yandex.ru>
2022-12-10 12:18:57 +0700

    Add partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def inclination(count, one, two, many)
+    return many if (count % 100).between?(11, 14)
+
+    case count % 10
+    when 1 then one
+    when 2..4 then two
+    else
+      many
+    end
+  end
 end
