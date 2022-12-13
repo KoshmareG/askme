e4c25e3d26619bc6e915d85254c61fe2aafff4eb
KoshmareG <koshmareg@yandex.ru>
2022-12-13 18:56:52 +0700

    Add user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,47 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    user = User.create(user_params)
-    session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
 
-    redirect_to root_path, notice: 'Вы зарегистрированы'
+      redirect_to root_path, notice: 'Вы зарегистрированы'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'
+
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Ваши данные обновлены'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля формы'
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'Пользователь удален'
+  end
+
+  private
+
+  def user_params
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
