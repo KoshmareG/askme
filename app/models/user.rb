6b54045e4a8ac0ad156b14d43179df2cbeedaf84
KoshmareG <koshmareg@yandex.ru>
2022-12-14 21:41:36 +0700

    Add validations for header color

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   has_secure_password
 
   before_validation :downcase_nickname
-  before_save :default_header_color
+  before_validation :default_header_color
 
   validates :nickname, presence: true, uniqueness: true,
     length: { maximum: 40 }, format: { with: /\A\w+\z/ }
   validates :email, presence: true, uniqueness: true,
     format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :header_color, presence: true,
+    format: { with: /\A#[[:xdigit:]]{6}\z/ }
 
   private
 
