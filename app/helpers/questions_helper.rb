64d66be169f83f7cea5e9c99974720f79c8a8a8a
KoshmareG <koshmareg@yandex.ru>
2023-01-10 17:05:58 +0700

    Change display questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,8 +1,5 @@
 module QuestionsHelper
   def hashtag_parser(string)
-    string.split(HashTag::TEXT_HASH_TAG_REGEXP).
-      map { |text| text.match?(HashTag::HASH_TAG_REGEXP) ? link_to(text, hash_tag_path(text.delete('#').downcase)) : text }.
-      join.
-      html_safe
+    string.gsub(HashTag::HASH_TAG_REGEXP) { |text| link_to(text, hash_tag_path(text.delete('#').downcase)) }
   end
 end
--- app/models/hash_tag.rb
+++ app/models/hash_tag.rb
@@ -1,6 +1,5 @@
 class HashTag < ApplicationRecord
   HASH_TAG_REGEXP = /#[[:word:]-]+/
-  TEXT_HASH_TAG_REGEXP = /(#[[:word:]]+)/
 
   has_many :question_hash_tags
   has_many :questions, through: :question_hash_tags
